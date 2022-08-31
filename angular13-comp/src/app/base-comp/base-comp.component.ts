7b8d430dd416f179b2570af2b56324ca1c9b893f
Christopher Piscopio <chris@coinsource.net>
2022-08-31 13:00:04 -0500

    Angular 13 with module federation
    In this PR we have a working angular 13 webpage and module federation web component to be consumed by a shell application.
    
    *** Missing ***
    Shared resources

---
+++ angular13-comp/src/app/base-comp/base-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-comp',
+  templateUrl: './base-comp.component.html',
+  styleUrls: ['./base-comp.component.scss']
+})
+export class BaseCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular13-comp/webpack.config.js
@@ -0,0 +1,17 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+//MODIFICATION: Change the name
+module.exports = withModuleFederationPlugin({
+
+  name: 'comp',
+  exposes: {
+    './routes': './src/app/base-comp/base-comp.routes.ts',
+    './Module': './src/app/base-comp/base-comp.module.ts',
+    './HeaderComponent': './src/app/header-comp/header-comp.component.ts'
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
